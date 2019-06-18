89d80abbc5fb6e67ea77df49eb806dd5b108a1e3
brady <acheng_69@163.com>
2019-06-18 17:57:49 +0800

    工具类完成

--- web/src/main.js
+++ web/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 import './assets/css/style.scss'
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ web/src/views/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+    <div class="topbar">
+      topbar
+    </div>
+    <div class="nav">
+      nav
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<style scoped>
+
+</style>
