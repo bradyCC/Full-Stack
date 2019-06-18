d8f23a5effb1fcb037440317969d8ba0e19e3896
brady <acheng_69@163.com>
2019-06-18 14:24:26 +0800

    上传文件封装完成

--- admin/src/main.js
+++ admin/src/main.js
@@ -10,6 +10,22 @@ Vue.config.productionTip = false
 import http from './http'
 Vue.prototype.$http = http
 
+// 全局第一
+Vue.mixin({
+  computed: {
+    uploadUrl () {
+      return this.$http.defaults.baseURL + 'upload'
+    }
+  },
+  methods: {
+    getAuthHeaders () {
+      return {
+        Authorization: `Bearer ${localStorage.token || ''}`
+      }
+    }
+  }
+})
+
 new Vue({
   router,
   render: h => h(App)
